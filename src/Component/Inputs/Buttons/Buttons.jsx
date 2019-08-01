c57f8aa9180e776e216fadc0633c84030f4487c8
Piyush Mehta <me@piyushmehta.com>
2019-08-01 14:33:09 +0530

    Button animated
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- src/Component/Inputs/Buttons/Buttons.jsx
+++ src/Component/Inputs/Buttons/Buttons.jsx
@@ -5,9 +5,9 @@ import "./styles.scss";
 export default class Button extends Component {
   render() {
     return (
-      <div>
-        <button className={this.props.color}>{this.props.color}</button>
-      </div>
+      <a href className={`${this.props.color} `}>
+        {this.props.color}
+      </a>
     );
   }
 }
