0913af870d813d999afb87ec0d659b38aeab7f19
Piyush Mehta <me@piyushmehta.com>
2019-08-02 22:16:04 +0530

    input

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Button from "./Components/Inputs/Buttons/Buttons";
 import "./App.scss";
 import Container from "./Components/Layout/Container/Container";
 import Typography from "./Components/Data Display/Typography/Typography";
+import TextField from "./Components/Inputs/TextField/TextField";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Box color="darkTertiary" p={2} m={2.5} />
         <Box color="disabled" p={2} m={2.5} />
         <Button color="primaryButton">Button</Button>
+        <TextField>Input</TextField>
         <Container type="fluid" maxWidth="sm" />
         <Container type="fluid" maxWidth="md" />
         <Container type="fluid" maxWidth="xl" />
