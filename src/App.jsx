9d51dcbe9fea8d9d579a8fd97ff073ad15e4b8e2
Tilak Singh <1ms17is124@gmail.com>
2019-08-06 02:58:18 +0530

    Updated Media
    Signed-off-by: Tilak Singh <1ms17is124@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
       <Container type="fluid" maxWidth="md" />
       <Container type="fluid" maxWidth="xl" />
       <Container type="fixed" /> */}
-      <Media typeGet = "linear">HELLO</Media>
+      <Media typeGet = "linear">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Media>
       
       
     </div>
