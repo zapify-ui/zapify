814f22dc0754368fbfc35c81ea0514f659ee4fcf
Tilak Singh <1ms17is124@gmail.com>
2019-08-06 02:57:10 +0530

    Updated Media
    Signed-off-by: Tilak Singh <1ms17is124@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,31 @@ import "./App.scss";
 import Container from "./Components/Layout/Container/Container";
 import Typography from "./Components/Data Display/Typography/Typography";
 import TextField from "./Components/Inputs/TextField/TextField";
-import Media from "./Components/Layout/Media/Media"
+import Media from "./Components/Layout/Media/Media";
+
+
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <Typography variant="h1">Zapify</Typography>
-        <Box color="boxPrimary" p={2} m={2.5} />
-        <Box color="boxSecondary" p={2} m={2.5} />
-        <Box color="boxTertiary" p={2} m={2.5} />
-        <Box color="darkPrimary" p={2} m={2.5} />
-        <Box color="darkSecondary" p={2} m={2.5} />
-        <Box color="darkTertiary" p={2} m={2.5} />
-        <Box color="disabled" p={2} m={2.5} />
-        <Button color="primaryButton">Button</Button>
-        <TextField>Input</TextField>
-        <Container type="fluid" maxWidth="sm" />
-        <Container type="fluid" maxWidth="md" />
-        <Container type="fluid" maxWidth="xl" />
-        <Container type="fixed" />
-        <Media typeGet = "linear"/>
-      </div>
+    
+      {/* <Typography variant="h1">Zapify</Typography>
+      <Box color="boxPrimary" p={2} m={2.5} />
+      <Box color="boxSecondary" p={2} m={2.5} />
+      <Box color="boxTertiary" p={2} m={2.5} />
+      <Box color="darkPrimary" p={2} m={2.5} />
+      <Box color="darkSecondary" p={2} m={2.5} />
+      <Box color="darkTertiary" p={2} m={2.5} />
+      <Box color="disabled" p={2} m={2.5} />
+      <Button color="primaryButton">Button</Button>
+      <TextField>Input</TextField>
+      <Container type="fluid" maxWidth="sm" />
+      <Container type="fluid" maxWidth="md" />
+      <Container type="fluid" maxWidth="xl" />
+      <Container type="fixed" /> */}
+      <Media typeGet = "linear"></Media>
+      
+      
     </div>
   );
 }
