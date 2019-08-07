203615cb7053b1e0e06b5b1cec37aaefddbbded7
Piyush Mehta <me@piyushmehta.com>
2019-08-07 23:12:38 +0530

    box

--- src/.storybook/config.js
+++ src/.storybook/config.js
@@ -1,7 +1,68 @@
-import { configure } from '@storybook/react';
+import { addParameters, configure } from "@storybook/react";
 
-const req = require.context('../stories', true, /\.stories\.js$/);
-
+const req = require.context("../stories", true, /\.stories\.js$/);
+// Option defaults:
+addParameters({
+  options: {
+    /**
+     * show story component as full screen
+     * @type {Boolean}
+     */
+    isFullscreen: true,
+    /**
+     * display panel that shows a list of stories
+     * @type {Boolean}
+     */
+    showNav: true,
+    /**
+     * display panel that shows addon configurations
+     * @type {Boolean}
+     */
+    showPanel: true,
+    /**
+     * where to show the addon panel
+     * @type {('bottom'|'right')}
+     */
+    panelPosition: "bottom",
+    /**
+     * regex for finding the hierarchy separator
+     * @example:
+     *   null - turn off hierarchy
+     *   /\// - split by `/`
+     *   /\./ - split by `.`
+     *   /\/|\./ - split by `/` or `.`
+     * @type {Regex}
+     */
+    hierarchySeparator: /\/|\./,
+    /**
+     * regex for finding the hierarchy root separator
+     * @example:
+     *   null - turn off multiple hierarchy roots
+     *   /\|/ - split by `|`
+     * @type {Regex}
+     */
+    hierarchyRootSeparator: /\|/,
+    /**
+     * sidebar tree animations
+     * @type {Boolean}
+     */
+    sidebarAnimations: true,
+    /**
+     * enable/disable shortcuts
+     * @type {Boolean}
+     */
+    enableShortcuts: true,
+    /**
+     * show/hide tool bar
+     * @type {Boolean}
+     */
+    isToolshown: true,
+    /**
+     * theme storybook, see link below
+     */
+    theme: undefined
+  }
+});
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
--- src/stories/box.stories.js
+++ src/stories/box.stories.js
@@ -2,7 +2,6 @@
 
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
 import Box from "../components/Box";
 
 storiesOf("Box", module).add("color padding and margin", () => (
