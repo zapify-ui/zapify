07a96fdd7e00d9421cf89ddf33241184e7d3a57c
Piyush Mehta <me@piyushmehta.com>
2019-08-08 02:31:04 +0530

    button

--- src/components/index.js
+++ src/components/index.js
@@ -6,3 +6,4 @@ export { default as TextField } from "./TextField";
 export { default as Container } from "./Container";
 export { default as Grid } from "./Grid";
 export { default as Media } from "./Media";
+export { default as Switch } from "./Switch";
