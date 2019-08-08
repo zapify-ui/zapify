eefdf3aff4af58e21359d5c096bf4bf000de6d13
vinaybiradar1717 <vinaybiradar1717@gmail.com>
2019-08-08 18:43:50 +0530

    gridspacing modified to functional component
    Signed-off-by: vinaybiradar1717 <vinaybiradar1717@gmail.com>

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,55 +1,60 @@
-// /* eslint-disable react/destructuring-assignment */
-// /* eslint-disable react/button-has-type */
-// /* eslint-disable react/prefer-stateless-function */
+/* eslint-disable react/no-access-state-in-setstate */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/label-has-for */
 
-// import React from "react";
-// import Proptypes from "prop-types";
-// import "./Grid.css";
+import React from "react";
+import Proptypes from "prop-types";
+import "./Grid.css";
 
-// export default function GridSpacing(){
-  
+function GridSpacing(props){
+    const nBox = props.nBox;
+    const m = props.m;
+    return (
+      <div className="GridSpacing">
+       {<CreateTable nBox={nBox} m={m}/>}
+      </div>
+    );
+}
 
-//   const createTable = props => {
-//     let table = [];
-//     const nBox = props.nBox;
-//     const m = props.m;
-//     console.log(nBox);
-//     // Outer loop to create parent
-//     for (let i = 0; i < 1; i++) {
-//       let children = [];
-//       //Inner loop to create children
-//       for (let j = 0; j < nBox; j++) {
-//         children.push(
-//           <td>
-//             <div
-//               className="GridBox"
-//               style={{
-//                 margin: `${m}rem`
-//               }}
-//             >
-//               {props.children}
-//             </div>
-//           </td>
-//         );
-//       }
-//       //Create the parent and add the children
-//       table.push(
-//         <tbody>
-//           <tr>{children}</tr>
-//         </tbody>
-//       );
-//     }
-//     return table;
-//   };
-//     return (
-//       <div className="GridSpacing">
-//         <table>{this.createTable}</table>
-//       </div>
-//     );
+ function CreateTable(props) {
+    let table = [];
+    const nBox = props.nBox;
+    const m = props.m;
+    console.log(nBox);
+    // Outer loop to create parent
+    for (let i = 0; i < 1; i++) {
+      let children = [];
+      //Inner loop to create children
+      for (let j = 0; j < nBox; j++) {
+        children.push(
+          <td>
+            <div
+              className="GridBox"
+              style={{
+                margin: `${m}rem`
+              }}
+            >
+              {props.children}
+            </div>
+          </td>
+        );
+      }
+      //Create the parent and add the children
+      table.push(
+        <tbody>
+          <tr>{children}</tr>
+        </tbody>
+      );
+    }
+    return table;
+  };
 
-// }
+GridSpacing.propType = {
+  nBox: Proptypes.number,
+  m: Proptypes.number
+};
 
-// GridSpacing.propType = {
-//   nBox: Proptypes.number,
-//   m: Proptypes.number
-// };
+export default GridSpacing;
