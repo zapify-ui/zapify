faea3091b601fbf3e9d3f287ffca80bb7f7a314a
Piyush Mehta <me@piyushmehta.com>
2019-08-09 01:45:27 +0530

    Appbar
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- src/components/AppBar/index.js
+++ src/components/AppBar/index.js
@@ -1,10 +1,43 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import PropTypes from "prop-types";
+import "./AppBar.css";
 
-function appBar(props) {
-  return <div></div>;
+function AppBar(props) {
+  const { title, numberOfLinks } = props;
+  const links = [];
+  for (let i = 0; i < numberOfLinks; i++) {
+    links.push(
+      <li className="link">
+        <a>{i}</a>
+      </li>
+    );
+  }
+
+  return (
+    <div id="navigation-bar">
+      <nav>
+        <ul>
+          <li>
+            <a href="#" id="logo">
+              {title}
+            </a>
+          </li>
+          {links}
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
-appBar.propTypes = {};
+AppBar.propTypes = {
+  title: PropTypes.string,
+  numberOfLinks: PropTypes.number
+};
 
-export default appBar;
+AppBar.defaultProps = {
+  title: "Zapify UI",
+  numberOfLinks: 3
+};
+
+export default AppBar;
