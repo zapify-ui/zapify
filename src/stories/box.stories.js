58b3201beeb90eb0e666215c9411c9b9ec24db78
Piyush Mehta <me@piyushmehta.com>
2019-08-09 01:53:30 +0530

    stories prettied

--- src/stories/box.stories.js
+++ src/stories/box.stories.js
@@ -6,8 +6,8 @@ import Box from "../components/Box";
 
 storiesOf("Box", module).add("color padding and margin", () => (
   <div>
-    <Box color="Primary" p={2} m={2.5}></Box>
-    <Box color="Secondary" p={2} m={2.5}></Box>
+    <Box color="Primary" p={2} m={2.5} />
+    <Box color="Secondary" p={2} m={2.5} />
     <Box color="dark" p={2} m={2.5} />
     <Box color="darkPrimary" p={2} m={2.5} />
     <Box color="darkSecondary" p={2} m={2.5} />
