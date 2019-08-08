0c4c204f4e37475c3d7969dedb82fc2fc78810e0
Piyush Mehta <me@piyushmehta.com>
2019-08-09 01:52:28 +0530

    media fix

--- src/components/Media/index.js
+++ src/components/Media/index.js
@@ -4,61 +4,74 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/label-has-for */
-import React,{Component} from 'react';
+import React, { Component } from "react";
 import Proptypes from "prop-types";
 import "./Media.css";
-function Media(){
-    return(<div>{<InnerFunc typeGet="nested"/>}</div>
-           
-    );
+function Media() {
+  return <div>{<InnerFunc typeGet="nested" />}</div>;
 }
 
 function InnerFunc(props) {
-    if(props.typeGet === "linear"){
-        return( 
-            <div className="Media">
-                <div>
-                    <img height={64} width={64} src="https://tinyurl.com/yylsuh97" alt="Generic placeholder"/>
-                </div>
-                <div>
-                    <h3>Media Heading</h3>
-                    <p>{props.children}</p>
-                </div>                   
-            </div>  
-             )
-  } else if(props.typeGet === "nested"){
-        return(
+  if (props.typeGet === "linear") {
+    return (
+      <div className="Media">
+        <div>
+          <img
+            height={64}
+            width={64}
+            src="https://tinyurl.com/yylsuh97"
+            alt="Generic placeholder"
+          />
+        </div>
+        <div>
+          <h3>Media Heading</h3>
+          <p>{props.children}</p>
+        </div>
+      </div>
+    );
+  } else if (props.typeGet === "nested") {
+    return (
+      <div>
+        <div className="Media">
+          <div>
+            <img
+              height={64}
+              width={64}
+              src="https://tinyurl.com/yylsuh97"
+              alt="Generic placeholder"
+            />
+          </div>
+          <div>
+            <h3>Outer Paragraph</h3>
+            <p>{props.children}</p>
+          </div>
+        </div>
+        <div style={{ paddingLeft: "10vw" }}>
+          <div className="Media">
             <div>
-                <div className="Media">
-                    <div>
-                        <img height={64} width={64} src="https://tinyurl.com/yylsuh97" alt="Generic placeholder"/>
-                    </div>
-                    <div>
-                        <h3>Outer Paragraph</h3>
-                        <p>{props.children}</p>    
-                    </div>
-                </div>       
-                <div style={{paddingLeft:'10vw'}}>
-                     <div className="Media">
-                     <div>
-                        <img height={64} width={64} src="https://tinyurl.com/yylsuh97" alt="Generic placeholder"/>  
-                     </div> 
-                     <div>
-                        <h3>Inner Paragraph</h3>
-                        <p>{props.children}</p>
-                    </div> 
-                     </div>   
-                </div>
-           </div>   
-        );
-    }else {
- console.log('Error');
-}
-return 
+              <img
+                height={64}
+                width={64}
+                src="https://tinyurl.com/yylsuh97"
+                alt="Generic placeholder"
+              />
+            </div>
+            <div>
+              <h3>Inner Paragraph</h3>
+              <p>{props.children}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    console.log("Error");
+  }
+  return;
 }
 
 Media.propType = {
-    typeGet: Proptypes.oneOf(['linear','nested'])
-  }
+  typeGet: Proptypes.oneOf(["linear", "nested"])
+};
 
-  export default Media;
+export default Media;
