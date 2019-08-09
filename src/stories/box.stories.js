ca8252d1bdeabc9ae2b54f09bc6e4ba4ba01f7a3
vinaybiradar1717 <vinaybiradar1717@gmail.com>
2019-08-09 18:19:51 +0530

    stories
    Signed-off-by: vinaybiradar1717 <vinaybiradar1717@gmail.com>

--- src/stories/box.stories.js
+++ src/stories/box.stories.js
@@ -4,13 +4,15 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import Box from "../components/Box";
 
-storiesOf("Box", module).add("color padding and margin", () => (
+storiesOf("Box", module)
+.add("color padding and margin", () => (
   <div>
-    <Box color="Primary" p={2} m={2.5} />
-    <Box color="Secondary" p={2} m={2.5} />
-    <Box color="dark" p={2} m={2.5} />
-    <Box color="darkPrimary" p={2} m={2.5} />
-    <Box color="darkSecondary" p={2} m={2.5} />
-    <Box color="disabled" p={2} m={2.5} />
-  </div>
-));
+    <Box color="Primary" textColor="black" p={2} m={2.5} />
+    <Box color="Secondary" textColor="white" p={2} m={2.5} />
+    <Box color="dark" textColor="white" p={2} m={2.5} />
+    <Box color="darkPrimary" textColor="white" p={2} m={2.5} />
+    <Box color="darkSecondary" textColor="white" p={2} m={2.5} />
+    <Box color="disabled" textColor="black" p={2} m={2.5} />
+  </div>))
+.add("User Custamisation", () => (<div></div>));
+
