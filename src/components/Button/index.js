dae3797bac85c052b1711744b489f2d1fff77de2
Piyush Mehta <me@piyushmehta.com>
2019-08-12 16:56:46 +0530

    buttong

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -4,11 +4,36 @@
 import React from "react";
 import Proptypes from "prop-types";
 import "./Button.css";
+import "../../styles/ColorSchema";
 
 function Button(props) {
+  const styles = {
+    position: "relative",
+    display: "inline-block",
+    verticalAlign: "middle",
+    padding: "0 0.3em",
+    height: "2.4em",
+    borderRadius: "5px",
+    lineHeight: "2.1",
+    overflow: "hidden",
+    fontSize: "1.1em",
+    margin: "10px 10px 10px 10px",
+    letterSpacing: "0.01em",
+    maxWidth: "100%",
+    textOverflow: "ellipsis",
+    whiteSpace: "normal",
+    userSelect: "none",
+    cursor: "pointer",
+    background:
+      props.color === "primaryColor"
+        ? "linear-gradient(60deg, #1badf8, #63da38)"
+        : Button.defaultProps.color,
+    textAlign: "center",
+    textDecoration: "none"
+  };
   return (
     <div>
-      <button className={props.color}>{props.children}</button>
+      <button style={styles}>{props.children}</button>
     </div>
   );
 }
@@ -16,4 +41,9 @@ function Button(props) {
 Button.propType = {
   color: Proptypes.string
 };
+
+Button.defaultProps = {
+  color: "#1badf8"
+};
+
 export default Button;
--- src/stories/button.stories.js
+++ src/stories/button.stories.js
@@ -5,8 +5,12 @@ import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import Button from "../components/Button";
 
-storiesOf("Button", module).add("with color props", () => (
-  <Button onClick={action("button-click")} color="primaryButton">
-    Hello
-  </Button>
-));
+storiesOf("Button", module)
+  .add("with color props", () => (
+    <Button onClick={action("button-click")} color="primaryColor">
+      primaryColor
+    </Button>
+  ))
+  .add("With default Props", () => (
+    <Button onClick={action("button-click")}>With default Props</Button>
+  ));
