d17faf244132803d5ba5b291f33920d200af74b2
Piyush Mehta <me@piyushmehta.com>
2019-08-12 13:53:59 +0530

    Fix

--- src/stories/zapbar.stories.js
+++ src/stories/zapbar.stories.js
@@ -10,5 +10,14 @@ storiesOf("ZapBar", module)
   .addDecorator(withInfo)
   .addWithJSX("With Default Props", () => <ZapBar />)
   .addWithJSX("With User Customisation", () => (
-    <ZapBar color="#f0f0f0" linkColor="#692478" links={[{name: "Custom Link 1", url: "www.piyushmehta.com"},{name: "Custom Link 2", url: "www.piyushmehta.com"}]} logoColor="#be1e6b" title="Brand Name"/>
+    <ZapBar
+      color="#f0f0f0"
+      linkColor="#692478"
+      links={[
+        { name: "Custom Link 1", url: "www.piyushmehta.com" },
+        { name: "Custom Link 2", url: "www.piyushmehta.com" }
+      ]}
+      logoColor="#be1e6b"
+      title="Brand Name"
+    />
   ));
