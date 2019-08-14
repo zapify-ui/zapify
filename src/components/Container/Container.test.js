8dfb3c21e95818b65ee7abecf8af43d2cfd37f93
meer sawood <sawoodrocket@gmail.com>
2019-08-14 09:01:41 +0530

    media and container test cases passes

---
+++ src/components/Container/Container.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Container from './index';
+
+test('Container tests', () => {
+    const component = renderer.create(
+        <Container > </Container>
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
