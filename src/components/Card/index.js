2c2e5cd89247387e18ac1fe53b1f4ff4f4024de0
vinaybiradar1717 <vinaybiradar1717@gmail.com>
2019-08-14 22:59:59 +0530

    card created
    Signed-off-by: vinaybiradar1717 <vinaybiradar1717@gmail.com>

---
+++ src/components/Card/index.js
@@ -0,0 +1,81 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/label-has-for */
+/* eslint-disable linebreak-style */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/prefer-stateless-function */
+import React from "react";
+import "./Card.css";
+
+function Card(props) {
+    return(
+        <div className="Card">
+           <div className="column"> 
+                <div className="Card-detail">
+                        <h1>Title</h1>
+                        <p>Sed ut perspiciatis unde omnis iste natus
+                         error sit voluptatem accusantium doloremque 
+                         laudantium, totam rem aperiam, eaque ipsa quae
+                          ab illo inventore veritatis et quasi architecto
+                           beatae vitae dicta sunt explicabo. Nemo enim 
+                           ipsam voluptatem quia voluptas sit aspernatur 
+                           aut odit aut fugit, sed quia consequuntur magni 
+                           dolores eos qui ratione voluptatem sequi nesciunt. 
+                           Neque porro quisquam est, qui dolorem ipsum quia
+                            dolor sit amet,</p>
+                        <button>Click </button>
+                </div>
+           </div>
+           <div className="column"> 
+                <div className="Card-detail">
+                        <h1>Title</h1>
+                        <p>Sed ut perspiciatis unde omnis iste natus
+                         error sit voluptatem accusantium doloremque 
+                         laudantium, totam rem aperiam, eaque ipsa quae
+                          ab illo inventore veritatis et quasi architecto
+                           beatae vitae dicta sunt explicabo. Nemo enim 
+                           ipsam voluptatem quia voluptas sit aspernatur 
+                           aut odit aut fugit, sed quia consequuntur magni 
+                           dolores eos qui ratione voluptatem sequi nesciunt. 
+                           Neque porro quisquam est, qui dolorem ipsum quia
+                            dolor sit amet,</p>
+                        <button>Click </button>
+                </div>
+           </div>
+           <div className="column"> 
+                <div className="Card-detail">
+                        <h1>Title</h1>
+                        <p>Sed ut perspiciatis unde omnis iste natus
+                         error sit voluptatem accusantium doloremque 
+                         laudantium, totam rem aperiam, eaque ipsa quae
+                          ab illo inventore veritatis et quasi architecto
+                           beatae vitae dicta sunt explicabo. Nemo enim 
+                           ipsam voluptatem quia voluptas sit aspernatur 
+                           aut odit aut fugit, sed quia consequuntur magni 
+                           dolores eos qui ratione voluptatem sequi nesciunt. 
+                           Neque porro quisquam est, qui dolorem ipsum quia
+                            dolor sit amet,</p>
+                        <button>Click </button>
+                </div>
+           </div>
+           <div className="column"> 
+                <div className="Card-detail">
+                        <h1>Title</h1>
+                        <p>Sed ut perspiciatis unde omnis iste natus
+                         error sit voluptatem accusantium doloremque 
+                         laudantium, totam rem aperiam, eaque ipsa quae
+                          ab illo inventore veritatis et quasi architecto
+                           beatae vitae dicta sunt explicabo. Nemo enim 
+                           ipsam voluptatem quia voluptas sit aspernatur 
+                           aut odit aut fugit, sed quia consequuntur magni 
+                           dolores eos qui ratione voluptatem sequi nesciunt. 
+                           Neque porro quisquam est, qui dolorem ipsum quia
+                            dolor sit amet,</p>
+                        <button>Click </button>
+                </div>
+           </div>
+        </div>
+    )
+}
+
+export default Card;
