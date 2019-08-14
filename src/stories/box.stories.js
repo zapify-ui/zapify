d67b15f64137691b1f77c795c7a0b4dfd0aea201
Piyush Mehta <me@piyushmehta.com>
2019-08-15 01:28:52 +0530

    box refactored

--- src/stories/box.stories.js
+++ src/stories/box.stories.js
@@ -1,18 +1,58 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
 import React from "react";
-import { storiesOf } from "@storybook/react";
+import { storiesOf, addDecorator } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
 import Box from "../components/Box";
+import Typography from "../components/Typography";
 
 storiesOf("Box", module)
-  .add("color padding and margin", () => (
+  .addDecorator(withInfo)
+  .addWithJSX("color primary Text Color black", () => (
     <div>
+      <center>
+        <Typography>color primary Text Color black</Typography>
+      </center>{" "}
       <Box color="Primary" textColor="black" p={2} m={2.5} />
-      <Box color="Secondary" textColor="white" p={2} m={2.5} />
-      <Box color="dark" textColor="white" p={2} m={2.5} />
-      <Box color="darkPrimary" textColor="white" p={2} m={2.5} />
-      <Box color="darkSecondary" textColor="white" p={2} m={2.5} />
-      <Box color="disabled" textColor="black" p={2} m={2.5} />
     </div>
   ))
-  .add("User Customisation", () => <div />);
+  .addWithJSX("Color Secondary Text color White", () => (
+    <div>
+      <center>
+        <Typography>Color Secondary Text color White</Typography>
+      </center>{" "}
+      <Box color="Secondary" textColor="white" p={2} m={2.5} />
+    </div>
+  ))
+  .addWithJSX("Color Dark Text color white", () => (
+    <div>
+      <center>
+        <Typography>Color Dark Text color white</Typography>
+      </center>{" "}
+      <Box color="dark" textColor="white" p={2} m={2.5} />
+    </div>
+  ))
+  .addWithJSX("Color DarkPrimary with textColor White", () => (
+    <div>
+      <center>
+        <Typography>Color DarkPrimary with textColor White</Typography>
+      </center>{" "}
+      <Box color="darkPrimary" textColor="white" p={2} m={2.5} />
+    </div>
+  ))
+  .addWithJSX("Color DarkSecondary with textColor White", () => (
+    <div>
+      <center>
+        <Typography>Color DarkSecondary with textColor White</Typography>
+      </center>{" "}
+      <Box color="darkSecondary" textColor="white" p={2} m={2.5} />
+    </div>
+  ))
+  .addWithJSX("Color Disabled with textColor Black", () => (
+    <div>
+      <center>
+        <Typography>Color Disabled with textColor Black</Typography>
+      </center>{" "}
+      <Box color="disabled" textColor="black" p={2} m={2.5} />
+    </div>
+  ));
