6d02fd97c256aebd208ea1b58fdc68fd0c1c6c9f
Piyush Mehta <me@piyushmehta.com>
2019-08-17 01:08:37 +0530

    WithInfo removed due to issue

--- src/stories/card1.stories.js
+++ src/stories/card1.stories.js
@@ -2,11 +2,6 @@
 
 import React from "react";
 import { storiesOf, addDecorator } from "@storybook/react";
-import { withInfo } from "@storybook/addon-info";
 import Card1 from "../components/Card1/index";
 
-storiesOf("Card1", module)
-  .addDecorator(withInfo)
-  .addWithJSX("Card", () => (
-       <Card1 />
- ));
+storiesOf("Card1", module).addWithJSX("Card", () => <Card1 />);
