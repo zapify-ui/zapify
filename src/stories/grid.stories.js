2dcc8e0d995e3ace3fb71f0bd67d196cb3dc326f
meer sawood <sawoodrocket@gmail.com>
2019-08-17 18:04:53 +0530

    Modified Grid Component

--- src/stories/grid.stories.js
+++ src/stories/grid.stories.js
@@ -3,11 +3,139 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import Grid from "../components/Grid";
+import InfoCard from "../components/InfoCard/index"
 
-storiesOf("Grid", module).add("with h1", () => (
-  <div>
-    <Grid nBox={4} m={3} />
-    <Grid nBox={3} m={3} />
-    <Grid nBox={2} m={3} />
-  </div>
-));
+storiesOf("Grid", module).addWithJSX("Grid with 16 Columns ", () => 
+
+<div>
+<Grid type="grid grid_16">
+  
+        <div className="col col-1">01</div>
+        <div className="col col-1">01</div>
+        <div className="col col-1">01</div>
+        <div className="col col-1">01</div>
+        <div className="col col-1">01</div>
+        <div className="col col-1">01</div>
+        <div className="col col-1">01</div>
+        <div className="col col-1">01</div>
+        <div className="col col-1">01</div>
+        <div className="col col-1">01</div>
+        <div className="col col-1">01</div>
+        <div className="col col-1">01</div>
+        <div className="col col-1">01</div>
+        <div className="col col-1">01</div>
+        <div className="col col-1">01</div>
+        <div className="col col-1">01</div>
+</Grid>
+<Grid type="grid grid_16">
+            <div className="col col-2">02</div>
+            <div className="col col-2">02</div>
+            <div className="col col-2">02</div>
+            <div className="col col-2">02</div>
+            <div className="col col-2">02</div>
+            <div className="col col-2">02</div>
+            <div className="col col-2">02</div>
+            <div className="col col-2">02</div>
+</Grid>
+<Grid type="grid grid_16">
+            <div className="col col-4">04</div>
+            <div className="col col-4">04</div>
+            <div className="col col-4">04</div>
+            <div className="col col-4">04</div>
+</Grid>
+<Grid type="grid grid_16">
+            <div className="col col-8">08</div>
+            <div className="col col-8">08</div>
+</Grid>
+<Grid type="grid grid_16">
+            <div className="col col-16">16</div>
+</Grid>
+    </div>
+).addWithJSX("Grid with 12 Columns" ,()=>
+<div>
+<Grid type="grid grid_12">
+        <div className="col col-1">01</div>
+        <div className="col col-1">01</div>
+        <div className="col col-1">01</div>
+        <div className="col col-1">01</div>
+        <div className="col col-1">01</div>
+        <div className="col col-1">01</div>
+        <div className="col col-1">01</div>
+        <div className="col col-1">01</div>
+        <div className="col col-1">01</div>
+        <div className="col col-1">01</div>
+        <div className="col col-1">01</div>
+        <div className="col col-1">01</div>
+</Grid>
+<Grid type="grid grid_12">
+    
+        <div className="col col-2">02</div>
+        <div className="col col-2">02</div>
+        <div className="col col-2">02</div>
+        <div className="col col-2">02</div>
+        <div className="col col-2">02</div>
+        <div className="col col-2">02</div>
+</Grid>
+<Grid type="grid grid_12">
+        <div className="col col-3">03</div>
+        <div className="col col-3">03</div>
+        <div className="col col-3">03</div>
+        <div className="col col-3">03</div>
+</Grid>
+        <Grid type="grid grid_12">
+        <div className="col col-4">04</div>
+        <div className="col col-4">04</div>
+        <div className="col col-4">04</div>
+    </Grid>
+        <Grid type="grid grid_12">
+        <div className="col col-6">06</div>
+        <div className="col col-6">06</div>
+    </Grid>
+        <Grid type="grid grid_12">
+        <div className="col col-12">12</div>
+    </Grid>
+        <Grid type="grid grid_12">
+        <div className="col col-9">09</div>
+        <div className="col col-3">03</div>
+    </Grid>
+</div>
+).addWithJSX("Grid with 8 Columns", ()=>
+    <div>
+        <Grid type="grid grid_8">
+            <div className="col col-1">01</div>
+            <div className="col col-1">01</div>
+            <div className="col col-1">01</div>
+            <div className="col col-1">01</div>
+            <div className="col col-1">01</div>
+            <div className="col col-1">01</div>
+            <div className="col col-1">01</div>
+            <div className="col col-1">01</div>
+            
+        </Grid>
+        <Grid type="grid grid_8">
+
+            <div className="col col-2">02</div>
+            <div className="col col-2">02</div>
+            <div className="col col-2">02</div>
+            <div className="col col-2">02</div>
+           
+        </Grid>
+        <Grid type="grid grid_8">
+            <div className="col col-4">04</div>
+            <div className="col col-4">04</div>
+           
+        </Grid>
+        <Grid type="grid grid_8">
+            <div className="col col-8">08</div>
+            
+        </Grid>
+    </div>
+).addWithJSX("Grid with Card",()=>
+<div>
+    <Grid type="grid grid_8">
+            <div className="col col-4"><InfoCard/></div>
+            <div className="col col-4"><InfoCard/></div>
+    </Grid>
+</div>
+
+);
