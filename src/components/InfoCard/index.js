5b6c07db2703cec5ef114fca0e5b5f45a414bc31
Piyush Mehta <me@piyushmehta.com>
2019-08-17 21:19:40 +0530

    Prettier

--- src/components/InfoCard/index.js
+++ src/components/InfoCard/index.js
@@ -8,26 +8,20 @@ import React from "react";
 import "./InfoCard.css";
 
 function InfoCard(props) {
-    return (
-
-        <div className="icard icard-1">
-
-            <h3>Title</h3>
-            <p>Sed ut perspiciatis unde omnis iste natus
-             error sit voluptatem accusantium doloremque
-             laudantium, totam rem aperiam, eaque ipsa quae
-              ab illo inventore veritatis et quasi architecto
-               beatae vitae dicta sunt explicabo. Nemo enim
-               ipsam voluptatem quia voluptas sit aspernatur
-               aut odit aut fugit, sed quia consequuntur magni
-               dolores eos qui ratione voluptatem sequi nesciunt.
-               Neque porro quisquam est, qui dolorem ipsum quia
-                            dolor sit amet,</p>
-
-        </div>
-
-
-    )
+  return (
+    <div className="icard icard-1">
+      <h3>Title</h3>
+      <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
+        quia dolor sit amet,
+      </p>
+    </div>
+  );
 }
 
 export default InfoCard;
