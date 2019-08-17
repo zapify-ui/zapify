6916bbdb07118f9519bb232b805d9ff86655c083
Piyush Mehta <me@piyushmehta.com>
2019-08-17 21:19:32 +0530

    Prettier

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,25 +1,20 @@
 import Proptypes from "prop-types";
 import React from "react";
-import "./Grid.css"
+import "./Grid.css";
 // eslint-disable-next-line import/imports-first
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/button-has-type */
 /* eslint-disable react/prefer-stateless-function */
 
-function Grid(props){
-
-
-
-
-    return(
-     <div>
-            <div className={props.type}>{props.children}</div>
-     </div>
-    )
+function Grid(props) {
+  return (
+    <div>
+      <div className={props.type}>{props.children}</div>
+    </div>
+  );
 }
 
 Grid.propType = {
-    type: Proptypes.string
-
+  type: Proptypes.string
 };
 export default Grid;
