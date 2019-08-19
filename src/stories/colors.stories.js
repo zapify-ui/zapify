29256939f98726050c7554063a4187aa53ebfaea
Piyush Mehta <me@piyushmehta.com>
2019-08-19 23:24:20 +0530

    COlor component story also available

--- src/stories/colors.stories.js
+++ src/stories/colors.stories.js
@@ -2,41 +2,6 @@
 
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { COLORS } from "../styles/ColorSchema";
+import Colors from "../components/Colors";
 
-storiesOf("Colors", module).addWithJSX("Pallete", () => (
-  <center>
-    <div
-      style={{
-        backgroundColor: COLORS.black,
-        maxWidth: "190px",
-        color: COLORS.white,
-        borderRadius: 10,
-        height: "70px",
-        textAlign: "center",
-        padding: "1em 1em 1em 1em",
-        margin: "1em 1em 1em 1em",
-        borderStyle: "solid",
-        borderColor: COLORS.white
-      }}
-    >
-      Black Color
-    </div>
-    <div
-      style={{
-        backgroundColor: COLORS.black,
-        maxWidth: "190px",
-        color: COLORS.white,
-        borderRadius: 10,
-        height: "70px",
-        textAlign: "center",
-        padding: "1em 1em 1em 1em",
-        borderStyle: "solid",
-        margin: "1em 1em 1em 1em",
-        borderColor: COLORS.white
-      }}
-    >
-      Black Color
-    </div>
-  </center>
-));
+storiesOf("Colors", module).addWithJSX("Pallete", () => <Colors />);
