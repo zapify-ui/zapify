196c08a942c921dacf359f3213aa0704b0b6d6d0
Piyush Mehta <me@piyushmehta.com>
2019-08-22 11:08:12 +0530

    removed from index

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 // eslint-disable-next-line import/prefer-default-export
-export { default as Button } from "./Button";
 export { default as Box } from "./Box";
 export { default as Typography } from "./Typography";
 export { default as TextField } from "./TextField";
