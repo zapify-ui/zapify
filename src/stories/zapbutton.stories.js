7c283cd46bebf3b54aeb68bf5104c207a5162150
vinaybiradar1717 <vinaybiradar1717@gmail.com>
2019-08-23 00:16:08 +0530

    orangeborderbutton
    Signed-off-by: vinaybiradar1717 <vinaybiradar1717@gmail.com>

--- src/stories/zapbutton.stories.js
+++ src/stories/zapbutton.stories.js
@@ -15,7 +15,7 @@ storiesOf("ZapButton", module)
           <ZapButton variant="paperButton">Zap Button</ZapButton>
   </div>
 ))
-.addWithJSX("This is of variant-> funkgradientButton", () => (
+.addWithJSX("This is of variant-> funkgradientButton<number>", () => (
     <div>
          <center>
             <Typography>funkgradientButtons</Typography>
@@ -36,11 +36,12 @@ storiesOf("ZapButton", module)
             <ZapButton variant="darkgradientButton">Zap Button</ZapButton>
     </div>
   ))
-  .addWithJSX("This is of variant-> blackBorderButton", () => (
+  .addWithJSX("This is of variant-> <color>BorderButton", () => (
     <div>
          <center>
-            <Typography>blackBorderButton</Typography>
+            <Typography>BorderButtons</Typography>
          </center>
             <ZapButton variant="blackBorderButton">Zap Button</ZapButton>
+            <ZapButton variant="orangeBorderButton">Zap Button</ZapButton>
     </div>
   ));
