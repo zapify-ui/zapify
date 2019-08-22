fdf0397f6aa9781831796b5a8e60daa13d6f0dd8
vinaybiradar1717 <vinaybiradar1717@gmail.com>
2019-08-22 05:35:40 +0530

    dark gradirnt button added
    Signed-off-by: vinaybiradar1717 <vinaybiradar1717@gmail.com>

--- src/stories/zapbutton.stories.js
+++ src/stories/zapbutton.stories.js
@@ -4,8 +4,22 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import ZapButton from "../components/ZapButton";
+import Typography from "../components/Typography";
 
 storiesOf("ZapButton", module)
-.addWithJSX("This is of variant paperButton", () => (
-<ZapButton variant="paperButton">Zap Button</ZapButton>
-));
+.addWithJSX("This is of variant-> paperButton", () => (
+  <div>
+       <center>
+          <Typography>paperButton</Typography>
+       </center>
+          <ZapButton variant="paperButton">Zap Button</ZapButton>
+  </div>
+))
+.addWithJSX("This is of variant-> darkgradientButton", () => (
+    <div>
+         <center>
+            <Typography>darkgradientButton</Typography>
+         </center>
+            <ZapButton variant="darkgradientButton">Zap Button</ZapButton>
+    </div>
+  ));
