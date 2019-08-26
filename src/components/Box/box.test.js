c8211e441a4aa0286a60e65f16bddc47700803b5
Piyush Mehta <me@piyushmehta.com>
2019-08-27 00:01:06 +0530

    testing snapshot fixed

--- src/components/Box/box.test.js
+++ src/components/Box/box.test.js
@@ -3,7 +3,7 @@ import renderer from "react-test-renderer";
 import Box from "./index";
 
 test("box default test case", () => {
-  const component = renderer.create(<Box color="Primary" p={2} m={2.5} />);
+  const component = renderer.create(<Box color="primary" />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
