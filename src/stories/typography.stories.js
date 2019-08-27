b6b35787d7d0d6a9845dc63a4dab7cd9abf13906
Piyush Mehta <me@piyushmehta.com>
2019-08-28 00:23:43 +0530

    story fixed

--- src/stories/typography.stories.js
+++ src/stories/typography.stories.js
@@ -8,11 +8,31 @@ storiesOf("Typography", module)
   .addWithJSX("with default Props", () => (
     <Typography>With Default Props</Typography>
   ))
-  .addWithJSX("with h1", () => <Typography variant="h1">with h1</Typography>)
-  .addWithJSX("with h2", () => <Typography variant="h2">with h2</Typography>)
-  .addWithJSX("with h3", () => <Typography variant="h3">with h3</Typography>)
-  .addWithJSX("with h4", () => <Typography variant="h4">with h4</Typography>)
-  .addWithJSX("with h5", () => <Typography variant="h5">with h5</Typography>)
+  .addWithJSX("with h1", () => (
+    <Typography variant="h1" color="black">
+      with h1
+    </Typography>
+  ))
+  .addWithJSX("with h2", () => (
+    <Typography variant="h2" color="black">
+      with h2
+    </Typography>
+  ))
+  .addWithJSX("with h3", () => (
+    <Typography variant="h3" color="black">
+      with h3
+    </Typography>
+  ))
+  .addWithJSX("with h4", () => (
+    <Typography variant="h4" color="black">
+      with h4
+    </Typography>
+  ))
+  .addWithJSX("with h5", () => (
+    <Typography variant="h5" color="black">
+      with h5
+    </Typography>
+  ))
   .addWithJSX("with custom color", () => (
     <Typography variant="h2" color="red">
       with h2 and custom color prop
