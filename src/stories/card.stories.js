2ad03aa0b08b280a7b557a82c24288e5560aa229
Piyush Mehta <me@piyushmehta.com>
2019-08-27 22:20:54 +0530

    card props

--- src/stories/card.stories.js
+++ src/stories/card.stories.js
@@ -4,4 +4,10 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import Card from "../components/Card";
 
-storiesOf("Card", module).addWithJSX("Card", () => <Card />);
+storiesOf("Card", module)
+  .addWithJSX("Card with default Props", () => <Card />)
+  .addWithJSX("Card with custom props", () => (
+    <Card buttonText="buttonText" titleText="title">
+      Contents will be here!
+    </Card>
+  ));
