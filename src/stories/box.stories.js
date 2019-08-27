0dee25a242d7d0ba4c3ed000262ade7b923682b8
Piyush Mehta <me@piyushmehta.com>
2019-08-28 00:03:45 +0530

    box stories resolved

--- src/stories/box.stories.js
+++ src/stories/box.stories.js
@@ -23,44 +23,4 @@ storiesOf("Box", module)
       </center>{" "}
       <Box color="primary" />
     </div>
-  ))
-  .addWithJSX("Color Blue ", () => (
-    <div>
-      <center>
-        <Typography>Color Blue </Typography>
-      </center>{" "}
-      <Box color="Secondary" />
-    </div>
-  ))
-  .addWithJSX("Color Pink ", () => (
-    <div>
-      <center>
-        <Typography>Color Pink </Typography>
-      </center>{" "}
-      <Box color="Tertiary" />
-    </div>
-  ))
-  .addWithJSX("Color DarkPrimary", () => (
-    <div>
-      <center>
-        <Typography>Color DarkPrimary</Typography>
-      </center>{" "}
-      <Box color="darkPrimary" />
-    </div>
-  ))
-  .addWithJSX("Color DarkSecondary", () => (
-    <div>
-      <center>
-        <Typography>Color DarkSecondary</Typography>
-      </center>{" "}
-      <Box color="darkSecondary" />
-    </div>
-  ))
-  .addWithJSX("Color Disabled", () => (
-    <div>
-      <center>
-        <Typography>Color Disabled</Typography>
-      </center>{" "}
-      <Box color="disabled" />
-    </div>
   ));
